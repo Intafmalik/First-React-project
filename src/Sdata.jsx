c5d5201a6ac3bb8212735706d8be21620cafedae
project-demo <intafmalik731@gmail.com>
2024-05-27 22:30:06 +0545

    first react project first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdate = [
+    {
+        sname:"CIVIL WAR",
+        imgsrc:"https://image.tmdb.org/t/p/w400/9oYNFH479z9iZ3NCch7cns4zIIx.jpg",
+        title:"NEW RELEASED MOVIES AND WEB SERIES ",
+        links:"https://hdhub4u.nexus/civil-war-2024-english-webrip-full-movie/"
+    },
+    {
+        sname:"WANTED MAN",
+        imgsrc:"https://image.tmdb.org/t/p/w400/pmdgdb8biQLLzTjySZLPdK13KwM.jpg",
+        title:"NEW RELEASED MOVIES AND WEB SERIES ",
+        links:"https://hdhub4u.nexus/wanted-man-2024-hindi-bluray-full-movie/"
+    },
+    {
+        sname:"THE FALL GUY",
+        imgsrc:"https://image.tmdb.org/t/p/w400/e7olqFmzcIX5c23kX4zSmLPJi8c.jpg",
+        title:"NEW RELEASED MOVIES AND WEB SERIES ",
+        links:"https://hdhub4u.nexus/the-fall-guy-2024-hindi-webrip-full-movie/"
+    },
+    { 
+        sname:"LOOTERE",
+        imgsrc:"https://image.tmdb.org/t/p/original/6o0C7Jy6TKw1Y2tmW5W2qDEsEut.jpg",
+        title:"NEW NETFLIX MOVIES AND WEB SERIES ",
+        links:"https://hdhub4u.nexus/lootere-season-1-hindi-webrip-all-episodes/"
+    },
+    {
+        sname:"SHAITAAN",
+        imgsrc:"https://image.tmdb.org/t/p/w400/nCK46DFCOMInVfSLraWDEOhMciE.jpg",
+        title:"NEW RELEASED MOVIES AND WEB SERIES ",
+        links:"https://hdhub4u.nexus/shaitaan-2024-hindi-webrip-full-movie/"
+    },
+    {
+        sname:"copshop",
+        imgsrc:"https://image.tmdb.org/t/p/w400/szArp3dQlVajpsqpyPFVAFHFsa3.jpg",
+        title:"NEW RELEASED MOVIES AND WEB SERIES ",
+        links:"https://hdhub4u.nexus/copshop-2021-hindi-bluray-full-movie/"
+    }
+]
+
+export default Sdate;
